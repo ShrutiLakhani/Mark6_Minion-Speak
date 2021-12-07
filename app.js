91e73f134fde0ab75a8a603a4e3d3fdbe3a0a87d
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2021-12-07 15:50:31 +0530

    Changes in CSS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
+var txtInput = document.querySelector(".txt-input");
 var outputDiv = document.querySelector("#output");
 console.log(txtInput.value);
 //console.log(btnTranslate);
